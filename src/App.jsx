84ba6a5753cef7d60d8f6847514470af1823a28f
yolandaatt <yolandaatteyeh3@gmail.com>
2025-10-02 17:02:15 +0200

    added the funtionality for Word Count and Character Count

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [characterCount, setChracterCount] = useState(0);
+  const [wordCount, setWordCount] = useState(0);
+
+  const handleCountChange = (e) => {
+    const currentValue = e.target.value;
+    setChracterCount(currentValue.length);
+
+    const words = currentValue.split(/\s+/).filter((word) => word.length !== 0);
+    setWordCount(words.length);
+  }
+
   return (
     <>
     <div className="greeting-container">
       <h1>Welcome to the Word Counter</h1>
     </div>
     <div className="text-input-container">
-      <textarea name="count-area" id="count-area" cols="30" rows="10" />
+      <textarea name="count-area" id="count-area" cols="30" rows="10" onChange={handleCountChange}/>
     </div>
     <div className="result-container">
-      <p>Word Count: 5</p>
+      <p>Word Count: {wordCount}</p>
       <br />
-      <p>Character Count: 5</p>
+      <p>Character Count: {characterCount}</p>
     </div>
     </>
   )
